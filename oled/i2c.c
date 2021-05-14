ad7736b0ec0c246dc70c51957b3bb679919b05a8
jithu <jithushaji23@gmail.com>
2021-05-14 15:49:56 +0530

    updated

--- oled/i2c.c
+++ oled/i2c.c
@@ -35,37 +35,41 @@ void sda_start()
 
 void sda_stop()
 {
-  sda_end = 1;
+    sda_end = 1; 
 }
 
 void intr_ser(void) interrupt 3
 {
   
-   if ((scl == 0) && (sda_end == 1))
-      sda = 0;
-   
+   if (sda_end == 1)
+   {
+     scl = ~scl;
+     if (scl == 0)
+       sda = 0;
+      if (scl == 1)
+      {
+        sda = 1;
+        sda_end = 0;
+      }
+     
+   }
    if((scl == 0) && (sda_begin == 1) && (sda_ == 1) && (sda_bit <= 9))
      {
       sda = ((sda_data >> sda_bit++) & 0x01);
       if(sda_bit == 9)
         {
-          //sda_begin = 0;
           sda_ack = 1;
-          //sda = 1;      //ack bit
-          //sda_bit = 0;    
-          //goto here;
         }
       } 
-     if((sda_ack == 1) && (scl == 0))  // acknowledge
+   if((sda_ack == 1) && (scl == 0))  // acknowledge
     {
       sda = 1;
-      
       ack_done = 1;
       sda_ack = 0;
     }    
-    if(sda_begin == 1)
+   if(sda_begin == 1)
     {      
-    scl =(~scl);  // clock
+      scl =(~scl);  // clock
       if (ack_done == 1)
       {
         sda_begin = 0;
@@ -75,15 +79,14 @@ void intr_ser(void) interrupt 3
         
     }
 
-    //here:
+   
    if((scl == 1) && (sda_strt == 1))  // send start condition
     {  
       sda = 0;
       sda_strt = 0;
       sda_ = 1;
     }
-    
-   
+      
     
    if ((scl == 1) && (sda_end == 1))  // send stop condition
      {
--- oled/main.c
+++ oled/main.c
@@ -11,6 +11,6 @@ void main()
   sda_write(0xBA);
   sda_write(0x1A);
   sda_write(0xB1);
+  sda_write(0x71);
   sda_stop();
-  //while(1);
 }
